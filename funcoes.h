fbdae2d74fa76bb49198ac756118e5c981b04e0c
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-07-05 21:47:16 -0300

    Problema do ponteiro com struct externa

--- funcoes.h
+++ funcoes.h
@@ -3,3 +3,11 @@ void umaFuncao();
 void ponteiroEstrela();
 void ponteiro();
 void ponteiroArray();
+void teste();
+
+struct mapa {
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+typedef struct mapa MAPA;
--- sintaxe_basicao.c
+++ sintaxe_basicao.c
@@ -18,6 +18,8 @@ typedef struct{
     char address;
 } Pessoa;
 
+MAPA m;
+
 void main(){
     // Teste de comentário
     printf("*********************************************\n");
@@ -195,64 +197,9 @@ void main(){
      Pessoa Aluno;
      Aluno.id = 15;
      printf("%d", Aluno.id);
-     /** Exemplo de requisição POST **/
-     WSADATA wsa;
-     SOCKET s;
-     struct sockaddr_in server;
-     char *message , server_reply[2000];
-     int recv_size;
-     printf("\nInitialising Winsock...");
-    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
-    {
-        printf("Failed. Error Code : %d",WSAGetLastError());
-    }
-    printf("Initialised.\n");
-    //Create a socket
-    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
-    {
-        printf("Could not create socket : %d" , WSAGetLastError());
-    }
-    printf("Socket created.\n");
-    server.sin_addr.s_addr = inet_addr("192.168.100.148");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 80 );
-    if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        puts("connect error");
-    }
-    puts("Gril Connected");
-    strcat(message, "POST /voleur/receiver.php HTTP/1.1\n");
-    strcat(message, "Host: 127.0.0.1\n");
-    strcat(message, "Connection: close\n");
-    strcat(message, "Content-Type:application/octet-stream\n");
-    strcat(message, "Content-Encoding:binary\n");
-    strcat(message, "Content-Length:16\n");
-    strcat(message, "Accept-Charset: utf-8\n\n");
-    if( send(s , message , strlen(message) , 0) < 0)
-    {
-        puts("Send failed");
-    }
 
-    // My argument
-    char *data = "arg1=Hello";
-    send(s , data , strlen(data), 0);
-    puts("Data Send\n");
-
-    //Receive a reply from the server
-    if((recv_size = recv(s , server_reply , 2000 , 0)) == SOCKET_ERROR)
-    {
-        puts("recv failed");
-    }
-
-    puts("Reply received\n");
-    server_reply[recv_size] = '\0';
-    puts(server_reply);
-
-
-
-
-
-
+    /**  Contrário do que dizia o curso, não precisamos, por algum motivo mover ponteiro para structs de outro arquivo para usa-lo**/
+     printf("%d", m.colunas);
 
 }
 
