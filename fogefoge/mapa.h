bbfcbbd9c6df634432abb1d78345b6da495194f5
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-07-08 20:24:27 -0300

    fim foge foge e inicio c para EPTH

--- fogefoge/mapa.h
+++ fogefoge/mapa.h
@@ -1,8 +1,13 @@
+#ifndef _MAPA_H_
+#define _MAPA_H_
+
+
 #define HEROI '@'
 #define VAZIO '.'
 #define PAREDE_VERTICAL '|'
 #define PAREDE_HORIZONTAL '-'
 #define FANTASMA 'F'
+#define PILULA 'P'
 
 struct mapa {
 	char** matriz;
@@ -15,7 +20,6 @@ typedef struct mapa MAPA;
 void alocamapa(MAPA* m);
 void lemapa(MAPA* m);
 void liberamapa(MAPA* m);
-void imprimemapa(MAPA* m);
 
 struct posicao {
 	int x;
@@ -36,3 +40,5 @@ void andanomapa(MAPA* m, int xorigem, int yorigem,
 void copiamapa(MAPA* destino, MAPA* origem);
 
 int podeandar(MAPA* m, char personagem, int x, int y);
+
+#endif
